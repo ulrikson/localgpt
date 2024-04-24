7cf6c1a5067cfdca54a83c51fc4ca7b11aed5b60
Ulrikson <erik.ulrikson@gmail.com>
2024-04-24 14:44:14 +0200

    no errors

--- hugging_face.py
+++ hugging_face.py
@@ -4,9 +4,17 @@ model_name = "gpt2"
 model = AutoModelForCausalLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-prompt = "Once upon a time,"
+prompt = "I am the"
 input_ids = tokenizer.encode(prompt, return_tensors="pt")
 
-output = model.generate(input_ids, max_length=50, num_return_sequences=1)
+attention_mask = input_ids.ne(0).long()
+
+output = model.generate(
+    input_ids,
+    max_length=50,
+    num_return_sequences=1,
+    attention_mask=attention_mask,
+    pad_token_id=tokenizer.eos_token_id,
+)
 
 print(tokenizer.decode(output[0], skip_special_tokens=True))
